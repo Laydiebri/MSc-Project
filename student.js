2e77612748ea4ed1e7a5abe4da4e48f52c972de5
Zabrina Osborne <Laydiebri24@hotmail.com>
2021-08-28 16:55:54 +0100

    student.js updated

--- student.js
+++ student.js
@@ -15,23 +15,45 @@ class Student {
         this.id = id;
         this.name = name;
     }
+
+  	// Returns student details as a HTML table row.
+    tableRow() {
+        return `<tr><td>${this.id}</td><td>${this.name}</td></tr>`;
+        return `<tr><td>${this.id}</td><td>${this.name}</td></tr>`;
+    }
 }
 
 // An array of students.
 var students = [
-    new Student("001", "Dior Williams"), 
-    new Student("002", "Carter Wise"), 
-    new Student("003", "Atlas Wise")];
+    new Student("001", "Dior Walker"), 
+    new Student("002", "Carter Walker"), 
+    new Student("003", "Atlas Walker")];
 
 function printStudents() {
-    // Build text to display
-    var text = "";
+    // Build html for table.
+    var html = 
+        `<table border="1">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+            </tr>`;
+    
+            var html1 = 
+            `<table border="1">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                </tr>`;
     // Iterate over all the students
     for (var student of students) {
-        text = text + student.id + ": " + student.name + "<br>";
+        html += student.tableRow();
+        html1 += student.tableRow();
     }
+    // End html table.
+    html += `</table>`
+    html1 += `</table>`
     // Get the main element
     var main = document.getElementById("main");
-    // Set the innerHTML to text
-    main.innerHTML = text;
+    // Set the innerHTML to html
+    main.innerHTML = html;
 }
